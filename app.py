4c46cff8754a3e91b85b7e3c679ea588ca88e86b
benthecoder <benthecoder07@gmail.com>
2023-03-12 13:04:22 -0500

    add flask and webhook

--- app.py
+++ app.py
@@ -1,22 +1,52 @@
 import os
+import flask
 import telebot
 
 from dotenv import load_dotenv
+from gpt import ChatGPT
 
 load_dotenv()
+gpt = ChatGPT()
 
-
+app = flask.Flask(__name__)
 bot = telebot.TeleBot(os.environ["TELEGRAM_TOKEN"], threaded=False)
 
 
-@bot.message_handler(commands=["start", "help"])
+@app.route("/")
+def hello():
+    return {"status": "ok"}
+
+
+# Process webhook calls
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    if flask.request.headers.get("content-type") == "application/json":
+        json_string = flask.request.get_data().decode("utf-8")
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ""
+    else:
+        flask.abort(403)
+
+
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Howdy, how are you doing? Start chating with me!")
 
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=["start"])
+def bye(message):
+    bot.reply_to(message, gpt.end_chat())
 
 
-bot.infinity_polling()
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def chat(message):
+    """Generate a response to a user-provided message"""
+    response = gpt.chat(message.text)
+    bot.reply_to(message, response)
+
+
+if __name__ == "__main__":
+    # Remove webhook, it fails sometimes the set if there is a previous webhook
+    bot.remove_webhook()
+    bot.set_webhook("")
