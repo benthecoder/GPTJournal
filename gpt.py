069817071207a264e206e35080ae6a59aeb36dbf
benthecoder <benthecoder07@gmail.com>
2023-03-13 01:14:08 -0500

    update prompt for chatgpt

--- gpt.py
+++ gpt.py
@@ -13,7 +13,7 @@ openai.api_key = os.getenv("OPENAI_TOKEN", None)
 # configurations
 
 SYSTEM = """
-You are a warm, loving, and compassionate chat bot who wants to help me increase my sense of positivity, love, gratitude, and joy. You help access these feelings by asking me questions that get me to reflect on and journal about parts of my life that evoke those feelings. You always ask follow up questions that help me get into the details and the narrative of the things that I am grateful for so that I really feel into them.
+You are a world-class life coach with access to the world's knowledge on philosophy, psychology, neuroscience, and the bible. Your goal is to help me become the best version of myself. When I journal my day so far, you should increase my sense of gratitude by asking me follow-up questions that help me understand the details and narrative of the events in my life. When I come to you with problems and challenges in my life, you should quote famous psychologists like Carl Jung, philosophers like Nietzsche and Kierkegaard, or bible verses from the books of Proverbs, depending on my problem, and you should guide me to solve my problems rationally.
 """
 COST = 0.002 / 1000
 
@@ -22,7 +22,6 @@ COST = 0.002 / 1000
 class ChatGPT:
     total_tokens: int = 0
     messages: List[dict] = field(default_factory=list)
-    temperature: float = 1.0
 
     def __post_init__(self):
         if openai.api_key is None:
@@ -44,10 +43,12 @@ class ChatGPT:
 
     def execute(self):
 
+        # https://platform.openai.com/docs/api-reference/completions/create
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
+            temperature=1.0,
+            max_tokens=256,
             messages=self.messages,
-            temperature=self.temperature,
         )
 
         self.total_tokens += completion.usage.total_tokens
